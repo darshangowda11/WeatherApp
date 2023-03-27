db423b716e629514efad48838fb00c9d9e004b4a
Darshan <darshanrgs45@gmail.com>
2023-03-27 07:46:29 +0530

    current weather component added

--- flutter-weather-app/src/App.js
+++ flutter-weather-app/src/App.js
@@ -1,10 +1,31 @@
-import Search from './components/Search';
-import './App.css';
+import { useState } from "react";
+import Search from "./components/Search";
+import CurrentWeather from "./components/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import "./App.css";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    new Promise(currentWeatherFetch)
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      })
+      .catch(console.log);
+  };
+
   return (
-    <div className="App">
-     <Search/>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
