11853cb454e8de94a8708ce7eb3b9452483fe9af
Darshan <darshanrgs45@gmail.com>
2023-03-27 07:54:02 +0530

    added forecast component

--- flutter-weather-app/src/App.js
+++ flutter-weather-app/src/App.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Search from "./components/Search";
 import CurrentWeather from "./components/current-weather";
+import Forecast from "./components/forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -13,11 +15,17 @@ function App() {
     const currentWeatherFetch = fetch(
       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
 
-    new Promise(currentWeatherFetch)
+    Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
       })
       .catch(console.log);
   };
@@ -26,6 +34,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
