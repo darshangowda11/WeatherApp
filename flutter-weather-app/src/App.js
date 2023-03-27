47abe4317f05dfd7ad0ac4e265afc36a58efc714
Darshan <darshanrgs45@gmail.com>
2023-03-27 07:11:41 +0530

    search screen updated

--- flutter-weather-app/src/App.js
+++ flutter-weather-app/src/App.js
@@ -1,10 +1,10 @@
-
+import Search from './components/Search';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Darshan here
+     <Search/>
     </div>
   );
 }
---
+++ flutter-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "77c51e0c6cmsh57bc8f87f7b198dp11c82cjsn4d343ce14362",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "0c0bbf616f4f916d74422c61f56f6195";
